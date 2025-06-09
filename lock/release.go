6f26e9979a9df995e895a03948ebc8e8ea28cb05
CURZOLA Pierre <pierre.curzola@gmail.com>
2025-06-09 14:47:18 +0200

    feat: add a callback when lock is timeout

--- lock/release.go
+++ lock/release.go
@@ -16,3 +16,18 @@ func (l *EtcdLock) Release() error {
 	defer l.session.Close()
 	return l.mutex.Unlock(context.Background())
 }
+
+func (l *EtcdLock) ReleaseOnTimeout() error {
+	if l == nil {
+		return errgo.New("nil lock")
+	}
+	l.Lock()
+	defer l.Unlock()
+
+	defer l.session.Close()
+
+	if l.callbackBeforeRelease != nil {
+		l.callbackBeforeRelease()
+	}
+	return l.mutex.Unlock(context.Background())
+}
