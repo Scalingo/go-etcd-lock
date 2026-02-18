e86f174f14cc00432a48bf149acffa0bde4f13ed
Étienne Michon <etienne@scalingo.com>
2026-02-18 11:11:02 +0100

    refactor: golangci-lint auto-fixes

--- lock/release_test.go
+++ lock/release_test.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestRelease(t *testing.T) {
@@ -12,29 +13,29 @@ func TestRelease(t *testing.T) {
 	t.Run("After release a key should be lockable immediately", func(t *testing.T) {
 		lock, err := locker.Acquire("/lock-release", 10)
 		assert.NotNil(t, lock)
-		assert.NoError(t, err)
+		require.NoError(t, err)
 
 		err = lock.Release()
-		assert.NoError(t, err)
+		require.NoError(t, err)
 
 		lock, err = locker.Acquire("/lock-release", 10)
 		assert.NotNil(t, lock)
-		assert.NoError(t, err)
+		require.NoError(t, err)
 
 		err = lock.Release()
-		assert.NoError(t, err)
+		require.NoError(t, err)
 	})
 
 	t.Run("After expiration, release a lock shouldn't produce an error", func(t *testing.T) {
 		lock, _ := locker.Acquire("/lock-release-exp", 1)
-		time.Sleep(2)
+		time.Sleep(2 * time.Millisecond)
 		err := lock.Release()
-		assert.NoError(t, err)
+		require.NoError(t, err)
 	})
 
 	t.Run("Release a nil lock should not panic", func(t *testing.T) {
 		var lock *EtcdLock
 		err := lock.Release()
-		assert.NotNil(t, err)
+		require.Error(t, err)
 	})
 }
